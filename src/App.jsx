aea67212f4ba9002af26175df1e2cd5b67856971
Harikrishnan <133265565+Harikrishnan0809@users.noreply.github.com>
2024-09-16 10:47:23 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h2>Weather</h2>
+      </nav>
+      <p>
+        This article is about the atmospheric process. For the geological
+        process, see Weathering. For other uses, see Weather (disambiguation)
+        and Weather systems (disambiguation). "Fair weather" redirects here. For
+        other uses, see Fairweather (disambiguation). Part of a series on
+        Weather Temperate and polar seasons Tropical seasons Storms
+        Precipitation Topics Glossaries icon Weather portal vte Weather is the
+        state of the atmosphere, describing for example the degree to which it
+        is hot or cold, wet or dry, calm or stormy, clear or cloudy.[1] On
+        Earth, most weather phenomena occur in the lowest layer of the planet's
+        atmosphere, the troposphere,[2][3] just below the stratosphere. Weather
+        refers to day-to-day temperature, precipitation, and other atmospheric
+        conditions, whereas climate is the term for the averaging of atmospheric
+        conditions over longer periods of time.[4] When used without
+        qualification, "weather" is generally understood to mean the weather of
+        Earth.
+      </p>
+    </div>
+  );
+}
